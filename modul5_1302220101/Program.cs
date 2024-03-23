70ee442cfcffb6708d2d755bbf04839e36a71738
Yoga Fikri <yogarz354@gmail.com>
2024-03-23 08:24:03 +0700

    menambahkan class SimpleDataBase

--- modul5_1302220101/Program.cs
+++ modul5_1302220101/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302220101
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SimpleDataBase<float> SDB = new SimpleDataBase<float>();
+            SDB.AddNewData(13);
+            SDB.AddNewData(02);
+            SDB.AddNewData(22);
+            SDB.PrintAllData();
+        }
+    }
+}
